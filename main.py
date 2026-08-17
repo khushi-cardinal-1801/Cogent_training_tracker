faf641137ae07a3c715c40feddf1c67afad8feb5
Khushi Aggarwal <khushi.aggarwal@cardinaltsinc.net>
2026-08-17 18:53:06 +0530

    redis implemented and added into the application

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ from sqlalchemy.orm import Session
 obj=FastAPI()
 from auth import router
 from auth import token_verfication
+from redis_client import redis_client
+import json
 
 database_model.base.metadata.create_all(bind=engine)
 obj.include_router(router)
+
+@obj.get('/redis-test')
+
+async def redis_test():
+    await redis_client.set("test-key", "radis is working")
+    value=await redis_client.get("test-key")
+    return value
 
 @obj.get("/")
 
@@ -30,7 +39,7 @@ def get_db():
         
 def init_db():
     db=session_local()
-    count=db.query(database_model.Product).count 
+    count=db.query(database_model.Product).count()
     if count==0:
         for i in products:
             db.add(database_model.Product(**i.model_dump()))  
@@ -40,33 +49,96 @@ init_db()
 
 @obj.get("/products")
 
-def prod_name(page:int=1,
+async def prod_name(page:int=1,
               limit:int=5,
               email:str=Depends(token_verfication),
               db: Session=Depends(get_db)
               ):
     skip=(page-1)*limit
+    cache_key = f"products:page:{page}:limit:{limit}"
+    cached_products=await redis_client.get(cache_key)
+    if cached_products:
+        print("products is fetching from redis")
+        return {"data from redis: ",json.loads(cached_products)}
+    print("getting data from postgres sql")
+    
+        
     db_products=db.query(database_model.Product).offset(skip).limit(limit).all()
-    return db_products
+    product_data=[
+       { 
+            "id":product.id,
+            "name":product.name,
+            "quantity":product.quantity,
+            "price":product.price,
+            "category":product.category,
+            "description": product.description
+        }
+       for product in db_products
+    ]
+    await redis_client.set(cache_key,
+                           json.dumps(product_data),
+                           ex=180
+                           )
+    
+    return {"data from postgres":product_data}
+
 
 @obj.get("/product/{id}")
 
-def get_product(id:int, email:str=Depends(token_verfication), db:Session=Depends(get_db)):
+async def get_product(id:int, email:str=Depends(token_verfication), db:Session=Depends(get_db)):
+    cached_product=await redis_client.get(f"product:{id}")
     selected_product=db.query(database_model.Product).filter(database_model.Product.id==id).first()
-    if(selected_product):
-        return selected_product
-    return "product not found"
+    if cached_product:
+        return {"product is cache:",json.loads(cached_product)}
+    elif selected_product:
+        if(selected_product):
+            product_data={
+                "id":selected_product.id,
+                "name":selected_product.name,
+                "category":selected_product.category,
+                "price":selected_product.price,
+                "quantity":selected_product.quantity,
+                "description": selected_product.description
+            }
+            await redis_client.set(
+                f"product:{id}",
+                json.dumps(product_data),
+                ex=180
+            )
+            return {"getting data from postgres:":product_data}
+    else:    
+        return "product not found"
     
 
 @obj.post("/product")
-def app_prod(productss:Product, email:str=Depends(token_verfication),db:Session=Depends(get_db)):
-    db.add(database_model.Product(**productss.model_dump()))
+async def app_prod(productss:Product, email:str=Depends(token_verfication),db:Session=Depends(get_db)):
+    new_product=database_model.Product(**productss.model_dump())
+    db.add(new_product)
     db.commit()
-    return productss
+    db.refresh(new_product)
+    product_data= {
+        "id":new_product.id,
+        "name":new_product.name,
+        "quantity":new_product.quantity,
+        "category":new_product.category,
+        "price":new_product.price,
+        "description":new_product.description
+    }
+    await redis_client.set(
+        f"product:{product_data['id']}",
+        json.dumps(product_data),
+        ex=180
+        )
+    keys = await redis_client.keys("products:page:*")
+
+    if keys:
+        await redis_client.delete(*keys)
+
+    return product_data
     
     
 @obj.put("/product")
-def update_prod(id:int,productss:Product,email:str=Depends(token_verfication), db:Session=Depends(get_db)):
+async def update_prod(id:int,productss:Product,email:str=Depends(token_verfication), db:Session=Depends(get_db)):
     selected_product=db.query(database_model.Product).filter(database_model.Product.id==id).first()
     if selected_product:
         selected_product.name=productss.name
@@ -75,20 +147,48 @@ def update_prod(id:int,productss:Product,email:str=Depends(token_verfication), d
         selected_product.quantity=productss.quantity
         selected_product.description=productss.description
         db.commit()
+        db.refresh(selected_product)
         
-        return "product updated successfully"
-    
+        product_data= {
+                "id":selected_product.id,
+                "name":selected_product.name,
+                "quantity":selected_product.quantity,
+                "category":selected_product.category,
+                "price":selected_product.price,
+                "description":selected_product.description
+            }
+        await redis_client.set(
+            f"product:{product_data['id']}",
+            json.dumps(product_data),
+            ex=180
+            )
+        keys = await redis_client.keys("products:page:*")
+
+        if keys:
+            await redis_client.delete(*keys)
+        return {
+            "message": "Product updated",
+            "data": product_data
+}
     else:
-        return "product id didnt match"
+        return "product doesn't exist"        
+        
+    
         
         
 
 @obj.delete("/product")
-def delete_prod(id:int,email:str=Depends(token_verfication), db:Session=Depends(get_db)):
+async def delete_prod(id:int,email:str=Depends(token_verfication), db:Session=Depends(get_db)):
     selected_product=db.query(database_model.Product).filter(database_model.Product.id==id).first()
     if selected_product:
         db.delete(selected_product)
         db.commit()
+        await redis_client.delete(f"product:{id}")
+        keys = await redis_client.keys("products:page:*")
+        
+        if keys:
+            await redis_client.delete(*keys)
+    
         return "Product deleted successfully"
     else:
         return "Product id not found"
